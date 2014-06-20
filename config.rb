a88da5ea5f53c93325f1dc675f3d99a834e11e40
grimrose <grwin7@gmail.com>
2014-06-20 19:58:34 +0900

    Updated site

---
+++ config.rb
@@ -0,0 +1,20 @@
+
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/fonts"
+css_path = "/Users/grimrose/IdeaProjects/groovyProject/jggug-jun-2014/src/site/.cache/compass/stylesheets"
+
+# Local development paths
+sass_path = "/Users/grimrose/IdeaProjects/groovyProject/jggug-jun-2014/src/site/theme/sass"
+generated_images_path = "/Users/grimrose/IdeaProjects/groovyProject/jggug-jun-2014/src/site/.cache/compass/images"
+images_path = "/Users/grimrose/IdeaProjects/groovyProject/jggug-jun-2014/src/site/theme/images"
+fonts_path = "/Users/grimrose/IdeaProjects/groovyProject/jggug-jun-2014/src/site/theme/fonts"
+
+line_comments = false
+output_style = :compressed
